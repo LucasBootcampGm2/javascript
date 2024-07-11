6635237eec349e17e18b1676d40f3399a3e013fd
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-07-11 10:28:25 -0300

    feat(exercises.js): all resolve

--- exercises.js
+++ exercises.js
@@ -1,109 +1,132 @@
 
-// Declarar una variable de tipo string cuyo nombre represente de forma clara su contenido.
+//TP Variables JS
+
+// Exercise 1
 let firstName = "Lucas"
 console.log(firstName)
 
-// Declarar una variable de tipo number cuyo nombre represente de forma clara su contenido.
+// Exercise 2
 let age = 18
 console.log(age)
 
-// Declarar una variable de tipo boolean cuyo nombre represente de forma clara su contenido.
-const man = true
-console.log(man)
+// Exercise 3
+const isMan = true
+console.log(isMan)
 
-// Declarar dos variables de tipo string. Luego declarar una nueva variable cuyo valor sea la concatenación de las dos variables previamente creadas. 
+// Exercise 4
 let names = "Lucas"
 let lastName = " Cirillo Berardi" 
 let result = names + lastName
 console.log(result)
 
 
-// Declarar dos variables de tipo number.
+// Exercise 5
 let number1 = 1
 let number2 = 2
 
-//  Luego declarar una nueva variable cuyo valor sea la suma de las dos variables previamente creadas. 
-let addition = number1 = number2
+    // Exercise 5a  
+    let addition = number1 + number2
 
-// Luego declarar una nueva variable cuyo valor sea la resta de las dos variables previamente creadas. 
-let substraction = number1 - number2
+    // Exercise 5b
+    let substraction = number1 - number2
 
-// Luego declarar una nueva variable cuyo valor sea la multiplicación de las dos variables previamente creadas. 
-let multiplication = number1 * number2
+    // Exercise 5c
+    let multiplication = number1 * number2
 
-// Luego declarar una nueva variable cuyo valor sea la división de las dos variables previamente creadas. 
-let division = number1 / number2
+    // Exercise 5d
+    let division = number1 / number2
 
-// Luego declarar una nueva variable cuyo valor sea el módulo entre las dos variables previamente creadas. 
-let module = number1 % number2
+    // Exercise 5e
+    let module = number1 % number2
 
-// Declarar una variable de tipo number y otra de tipo string. Luego sumar ambas variables y guardar su resultado en una nueva variable. De qué tipo será la tercera variable?
+// Exercise 6
 let name3 = "Lucas"
 let number3 = 18
 let result2 = name3 + number3
 console.log(result2) // La tercera variable sera de tipo string
 
-// Declarar una variable de tipo booleano cuyo nombre represente su valor. Luego declarar una nueva variable cuyo valor sea opuesto a la variable previamente creada.
+// Exercise 7
 let trueValue = true
-let falseValue = true
+let falseValue =! trueValue
 
-// Crear una variable de tipo number. 
+// Exercise 8
 let number4 = 1
 console.log(number4)
 
-// Reasignar un nuevo valor a esa misma variable para que sea su valor + 1. 
-number4++ 
-console.log(number4) 
+    // Exercise 8a
+    number4++ 
+    console.log(number4) 
 
-// Repetir la operación anterior utilizando una estrategia diferente
-number4 += 1
+    // Exercise 8b
+    number4 += 1
 
-// Repetir ambas operaciones con operadores de resta, multiplicación y división.
-number4 -= 1 
-number4 *= 1 
-number4 /= 1 
+    // Exercise 8c
+    number4 -= 1 
+    number4 *= 1 
+    number4 /= 1 
+    number4 = number4 - 1
+    number4 = number4 * 1
+    number4 = number4 / 1
 
-// Repetir la operación anterior utilizando una estrategia diferente
-number4 = number4 - 1
-number4 = number4 * 1
-number4 = number4 / 1
-
-// Declarar una constante e intentar reasignarle otro valor luego de su declaración. Que output recibis?
+// Exercise 9
 const test = 2
-//const test = 3
+//test = 3
 console.log(test) // Recibo que ya esta declarada y, por tanto, me tira error
 
-// Crear dos variables de tipo string y dos variables de tipo number. 
+// Exercise 10
 let string1 = "Hola"
 let string2 = "Mundo"
 let int1 = 1
 let int2 = 2
 
-// Crear una nueva variable cuyo valor sea un string que incluya las cuatro variables previamente creadas. 
-let valor = string1 + string2 + int1 + int2
-console.log(valor)
+    // Exercise 10a
+    let valor = string1 + string2 + int1 + int2
+    console.log(valor)
 
-// Repetir el punto anterior utilizando una estrategia diferente.
-let valor2 = valor.concat()
-console.log(valor2)
+    // Exercise 10b
+    let valor2 = ""
+    valor2 = valor2.concat(string1 + " " + string2 + " " + int1 + " " + int2 + "soy valor 2")
+    console.log(valor2)
 
-// Crear dos variables de tipo number.
+// Exercise 11
 let number5 = 5
 let number6 = 6
 
-// Crear cuatro nuevas variables de tipo boolean comparando los dos valores utilizando operadores de comparación.
-let b1 = number5 - number6 == 0
-let b2 = number5 * number6 == 0
-let b3 = number5 / number6 == 0
-let b4 = number5 % number6 == 0
+    // Exercise 11a
+    let b1 = number5 - number6 == 0
+    let b2 = number5 * number6 == 0
+    let b3 = number5 / number6 == 0
+    let b4 = number5 % number6 == 0
 
-// Declarar cinco variables de tipo string cuyo valor contenga operaciones aritméticas. Intenta utilizar diferentes estrategias. 
-let string3 = "10 + 1" 
-let string4 = "10" + "+" + "1" 
-let string5 = ""
-let string6 
-let string7 
-// Crea un bloque de código que, utilizando variables con nombres representativos, calcule la cantidad de propina a dejar en un restaurante, basada en el total de la cuenta y el porcentaje de propina ingresado por el usuario.
-// Crea un bloque de código que calcule el precio final de un producto luego de aplicar un descuento. 
-// Crea un bloque de código que, tomando el año de nacimiento de una persona, devuelva su edad actual.
-// Crea un bloque de código que, definiendo las variables necesarias, calcule el área de un círculo, un cuadrado y un triángulo.
+// Exercise 12
+let string3 = `${10 + 5}`
+let string4 = `${10 - 7}`
+let string5 = `${10 / 5}`
+let string6 = `${10 * 2}`
+let string7 = `${10 % 3}`
+
+// Exercise 13
+let bill = 15000
+let bill_percent = prompt("Enter the percent number you would like to leave")
+let tip = bill * bill_percent / 100
+
+// Exercise 14
+let product_price = 20000
+let discount = 20
+let final_price = product_price - product_price * discount/100
+
+// Exercise 15
+let current_year = 2024
+const birth_year = prompt("Enter the year you were born")
+let current_age = current_year - birth_year
+
+// Exercise 16
+let circle_radio = prompt("Enter the radio of the circle")
+let circle_area = 3.14 * (r ** 2)
+
+let square_side = prompt("Enter the size of the square side")
+let square_area = square_side ** 2
+
+let triangle_base = prompt("Enter the size of the triangle base")
+let triangle_height = prompt("Enter the triangle height")
+let triangle_area = triangle_base * triangle_height / 2
