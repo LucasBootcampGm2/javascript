0733975551eca5e3bb6fdca513ed20556773a8b2
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-07-11 09:14:08 -0300

    feat(exercises.js): exercises 1 and 2 resolve

---
+++ exercises.js
@@ -0,0 +1,109 @@
+
+// Declarar una variable de tipo string cuyo nombre represente de forma clara su contenido.
+let firstName = "Lucas"
+console.log(firstName)
+
+// Declarar una variable de tipo number cuyo nombre represente de forma clara su contenido.
+let age = 18
+console.log(age)
+
+// Declarar una variable de tipo boolean cuyo nombre represente de forma clara su contenido.
+const man = true
+console.log(man)
+
+// Declarar dos variables de tipo string. Luego declarar una nueva variable cuyo valor sea la concatenación de las dos variables previamente creadas. 
+let names = "Lucas"
+let lastName = " Cirillo Berardi" 
+let result = names + lastName
+console.log(result)
+
+
+// Declarar dos variables de tipo number.
+let number1 = 1
+let number2 = 2
+
+//  Luego declarar una nueva variable cuyo valor sea la suma de las dos variables previamente creadas. 
+let addition = number1 = number2
+
+// Luego declarar una nueva variable cuyo valor sea la resta de las dos variables previamente creadas. 
+let substraction = number1 - number2
+
+// Luego declarar una nueva variable cuyo valor sea la multiplicación de las dos variables previamente creadas. 
+let multiplication = number1 * number2
+
+// Luego declarar una nueva variable cuyo valor sea la división de las dos variables previamente creadas. 
+let division = number1 / number2
+
+// Luego declarar una nueva variable cuyo valor sea el módulo entre las dos variables previamente creadas. 
+let module = number1 % number2
+
+// Declarar una variable de tipo number y otra de tipo string. Luego sumar ambas variables y guardar su resultado en una nueva variable. De qué tipo será la tercera variable?
+let name3 = "Lucas"
+let number3 = 18
+let result2 = name3 + number3
+console.log(result2) // La tercera variable sera de tipo string
+
+// Declarar una variable de tipo booleano cuyo nombre represente su valor. Luego declarar una nueva variable cuyo valor sea opuesto a la variable previamente creada.
+let trueValue = true
+let falseValue = true
+
+// Crear una variable de tipo number. 
+let number4 = 1
+console.log(number4)
+
+// Reasignar un nuevo valor a esa misma variable para que sea su valor + 1. 
+number4++ 
+console.log(number4) 
+
+// Repetir la operación anterior utilizando una estrategia diferente
+number4 += 1
+
+// Repetir ambas operaciones con operadores de resta, multiplicación y división.
+number4 -= 1 
+number4 *= 1 
+number4 /= 1 
+
+// Repetir la operación anterior utilizando una estrategia diferente
+number4 = number4 - 1
+number4 = number4 * 1
+number4 = number4 / 1
+
+// Declarar una constante e intentar reasignarle otro valor luego de su declaración. Que output recibis?
+const test = 2
+//const test = 3
+console.log(test) // Recibo que ya esta declarada y, por tanto, me tira error
+
+// Crear dos variables de tipo string y dos variables de tipo number. 
+let string1 = "Hola"
+let string2 = "Mundo"
+let int1 = 1
+let int2 = 2
+
+// Crear una nueva variable cuyo valor sea un string que incluya las cuatro variables previamente creadas. 
+let valor = string1 + string2 + int1 + int2
+console.log(valor)
+
+// Repetir el punto anterior utilizando una estrategia diferente.
+let valor2 = valor.concat()
+console.log(valor2)
+
+// Crear dos variables de tipo number.
+let number5 = 5
+let number6 = 6
+
+// Crear cuatro nuevas variables de tipo boolean comparando los dos valores utilizando operadores de comparación.
+let b1 = number5 - number6 == 0
+let b2 = number5 * number6 == 0
+let b3 = number5 / number6 == 0
+let b4 = number5 % number6 == 0
+
+// Declarar cinco variables de tipo string cuyo valor contenga operaciones aritméticas. Intenta utilizar diferentes estrategias. 
+let string3 = "10 + 1" 
+let string4 = "10" + "+" + "1" 
+let string5 = ""
+let string6 
+let string7 
+// Crea un bloque de código que, utilizando variables con nombres representativos, calcule la cantidad de propina a dejar en un restaurante, basada en el total de la cuenta y el porcentaje de propina ingresado por el usuario.
+// Crea un bloque de código que calcule el precio final de un producto luego de aplicar un descuento. 
+// Crea un bloque de código que, tomando el año de nacimiento de una persona, devuelva su edad actual.
+// Crea un bloque de código que, definiendo las variables necesarias, calcule el área de un círculo, un cuadrado y un triángulo.
