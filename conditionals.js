0e3e90bce2950b298791ed2038a5af1aa2ecad07
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-07-29 13:01:44 -0300

    feat(conditionals.js): create situations 1 and 2 for exercise 31

--- conditionals.js
+++ conditionals.js
@@ -100,7 +100,7 @@
         console.log("El numero es menor a 10")
     } else if (num5 > 50) {
         console.log("El numero es mayor a 50")
-    } else if (num5 >= 10 && num5 <= 50) {
+    } else {
         console.log("El numero es mayor a 10 pero menor que 50")
     }
 
@@ -176,28 +176,28 @@
 // Switch:
 
     // Crea un bloque de código que tome de referencia un número del 1 al 7. Utiliza una sentencia Switch para imprimir en consola el día de la semana correspondiente.
-    let switch_Day 
+    let switch_day 
     switch (new Date().getDay()) {
         case 0:
-            switch_Day = "Sunday";
+            switch_day = "Sunday";
             break;
         case 1:
-            switch_Day  = "Monday";
+            switch_day  = "Monday";
             break;
         case 2:
-            switch_Day  = "Tuesday";
+            switch_day  = "Tuesday";
             break;
         case 3:
-            switch_Day = "Wednesday";
+            switch_day = "Wednesday";
             break;
         case 4:
-            switch_Day  = "Thursday";
+            switch_day  = "Thursday";
             break;
         case 5:
-            switch_Day = "Friday";
+            switch_day = "Friday";
             break;
         case 6:
-            switch_Day = "Saturday";
+            switch_day = "Saturday";
       }
       console.log(switch_Day)
 
@@ -300,15 +300,124 @@
 // Operador ternario:
 
     // Crea un bloque de código que tome de referencia una variable ‘edad’. Utiliza el operador ternario para imprimir en consola si es o no mayor de edad.
-        
+    let edad = 33    
+    let isMayor = edad >= 18 ? "Es mayor de edad" : "Es menor de edad";
+    console.log(isMayor)
+
     // Crea un bloque de código que tome de referencia un número. Utiliza el operador ternario para determinar si es par o impar y muestra el resultado en consola.
+    let num11 = 13
+    let even = name11 % 2 == 0 ? "Es par" : "Es impar"
+    console.log(even)
+
     // Crea un bloque de código que tome de referencia dos números y utilice el operador ternario para determinar cual de los dos es el mayor. Luego imprime en consola el resultado.
+    let num12 = 2
+    let num13 = 5
+    let mayorNum = num12 > num13 ? `El ${num12} es mayor al ${num13}`: `El ${num13} es mayor al ${num12}`
+    console.log(mayorNum)
+
     // Crea un bloque de código que tome de referencia un número del 1 al 7 y utilice el operador ternario para imprimir en consola el día de la semana correspondiente.
+    let num15 = 6
+    let date = number15 === 1 ? "Es lunes": 
+               number15 === 2 ? "Es martes":
+               number15 === 3 ? "Es miercoles":
+               number15 === 4 ? "Es jueves":
+               number15 === 5 ? "Es viernes":
+               number15 === 6 ? "Es sabado":
+               number15 === 7 ? "Es domingo":
+               "Numero invalido";
+    console.log(date)
+
     // Crea un bloque de código que tome de referencia un número. Utiliza el operador ternario para determinar si es positivo, negativo o cero e imprime el resultado en consola.
+    let num16 = 3
+    let typeNum = num16 > 0 ? "Es positivo":
+                  num16 < 0 ? "Es negativo":
+                  "Es igual a 0";   
+    console.log(typeNum)
+
     // Crea un bloque de código que tome de referencia un número del 1 al 100 y utilice el operador ternario para determinar si es múltiplo de 3 y 5 para imprimir el resultado en consola.
+    let num17 = 15
+    let isMultiple1 = num17 % 3 === 0 && num17 % 5 === 0 ? `${num17} es multiplo de 3 y 5` : `${num17} no es multiplo de 3 y 5`
+    console.log(isMultiple1)
 
 // Prácticas: 
 
-// Crea un bloque de código que tome de referencia un número del 1 al 100 y, utilizando el tipo de condicional que prefieras, imprimir si es múltiplo de 2, 3 o 5 incluyendo las diferentes combinaciones posibles.
-// Crea un bloque de código que reciba un numero del 1 al 1000. Utiliza el tipo de condicional que prefieras para imprimir en consola si el numero es par o impar, si es divisible por 2, por 3 o por 5, y si es mayor o menor a 50.
-// Desarrolla 10 nuevas situaciones en donde requieras de utilizar condicionales para su resolución.
+    // Crea un bloque de código que tome de referencia un número del 1 al 100 y, utilizando el tipo de condicional que prefieras, imprimir si es múltiplo de 2, 3 o 5 incluyendo las diferentes combinaciones posibles.
+    let num18 = 14
+    let isMultiple2 = num18 % 2 == 0 && num18 % 3 == 0 && num18 % 5 == 0 ? "Es multiplo de 2 ,3 y 5":
+                      num18 % 2 == 0 && num18 % 3 == 0 ? "Es multiplo de 2 y 3":
+                      num18 % 2 == 0 && num18 % 5 == 0 ? "Es multiplo de 2 y 5":
+                      num18 % 3 == 0 && num18 % 5 == 0 ? "Es multiplo de 3 y 5":
+                      num18 % 2 == 0 ? "Es multiplo de 2":
+                      num18 % 3 == 0 ? "Es multiplo de 3":
+                      num18 % 5 == 0 ? "Es multiplo de 5":
+                      "No es multiplo de ninguno"
+
+    console.log(isMultiple2)
+
+    // Crea un bloque de código que reciba un numero del 1 al 1000. Utiliza el tipo de condicional que prefieras para imprimir en consola si el numero es par o impar, si es divisible por 2, por 3 o por 5, y si es mayor o menor a 50.
+    let num19 = 500
+    
+    if (num19 % 2 == 0) {
+        console.log("Es par")
+    } else {
+        console.log("Es impar")    
+    }
+    
+    if (num19 % 2 == 0 || num19 % 3 == 0 || num19 % 5 == 0){
+        console.log("Es divisible por 2, 3 o 5")   
+    } else {
+        console.log("No es divisible por 2, 3 o 5")
+    }
+
+    if (num19 > 50) {
+        console.log("Es mayor a 50")
+    } else if (num19 < 50) {
+        console.log("Es menor a 50")
+    } else {
+        console.log("Es igual a 50")
+    }
+
+    // Desarrolla 10 nuevas situaciones en donde requieras de utilizar condicionales para su resolución.
+        // Crea un bloque de codigo que utilice switch para verificar que dia es segun el numero ingresado.
+        let dayPrompt = prompt("Ingrese un numero entre 1 y 7")
+        let whatDay
+        switch (dayPrompt){
+            case 1:
+                whatDay = "Monday";
+                break;
+            case 2:
+                whatDay  = "Tuesday";
+                break;
+            case 3:
+                whatDay = "Wednesday";
+                break;
+            case 4:
+                whatDay  = "Thursday";
+                break;
+            case 5:
+                whatDay = "Friday";
+                break;
+            case 6:
+                whatDay = "Saturday";
+                break;
+            case 7:
+                whatDay = "Sunday"
+        }
+        console.log(switch_Day)
+
+        // Crea un bloque de codigo que verifique si dos variables, una ingresada y otra predefinida, son iguales en tipo y contenido.
+        let predifineNum = "14"
+        let num21 = prompt("Ingrese un numero o string")
+        if (predifineNum === num21) {
+            console.log("Es igual en contenido y tipo")
+        } else if (typeof(predifineNum)){}
+
+
+        // Crea un bloque de codigo que verifique si un numero es par, si es par que verifique si es mayor a 10, o si es impar, que verifique si es divisble por 5. Todo utilizando ternarios y lo imprima en la consola.
+        let num20 = 14
+        let isEven = num20 % 2 == 0 ? 
+                     (num20 > 10 ? "Es par y mayor a 10" : "Es par y menor a 10"): 
+                     (num20 % 5 == 0 ? "Es impar y es divisible por 5" : "Es impar pero no es divisible por 5");
+        console.log(isEven)
+        
+        
