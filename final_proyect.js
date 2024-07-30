ddc96d6970a46ae795d5080fc348117ca6d8f7d9
Lucas <lcirilloberardi@alumno.huergo.edu.ar>
2024-07-30 20:02:36 -0300

    fix(final_proyect): change proyect language

--- final_proyect.js
+++ final_proyect.js
@@ -2,26 +2,26 @@
 // Mostrar el resumen del pedido: Usar console.log para mostrar todos los detalles del pedido, incluyendo el costo total, el descuento aplicado, el impuesto, y el costo final.
 
 
-function gestionarPedido(nombre,edad,cantidadPedida, precioUnitario,metodoDePago,hora) {
-    saludarCliente(nombre)
-    esMayor(edad)
+function gestionarPedido(name,age,cantidadPedida, precioUnitario,metodoDePago,hora) {
+    saludarCliente(name)
+    esMayor(age)
     calcularCostoTotal(cantidadPedida,precioUnitario)
-    DeterminarDescuentoPorPorEdad(edad)
+    DeterminarDescuentoPorPorage(age)
     DeterminarDescuentoPorMetodoDePago(metodoDePago)
     mostrarHoraDePedido(hora)
-    calcularValorCubierto(descuentoEnEfectivo,descuentoPorEdad)
+    calcularValorCubierto(descuentoEnEfectivo,descuentoPorage)
     mostrarResumenDelPedido()
 }
 
-function saludarCliente(nombre){
-    return `Hola ${nombre}, bienvenido a Gm2, sera un gusto trabajar juntos!`
+function greetConsumer(name){
+    return `Hola ${name}, bienvenido a Gm2, sera un gusto trabajar juntos!`
 }
 
-function esMayor(edad) {
-    if (edad < 18) {
+function isMayor(age) {
+    if (age < 18) {
         console.warn("No puede beber bebidas alcoholicas")
     } else {
-        return "Es mayor de edad"
+        return "Es mayor de age"
     }
 }
 
@@ -29,10 +29,10 @@ function calcularCostoTotal(cantidadPedida,precioUnitario) {
     return cantidadPedida * precioUnitario
 } 
 
-function DeterminarDescuentoPorEdad(edad) {
-    if (edad > 60) {
-        let descuentoPorEdad = 0.1
-        return descuentoPorEdad
+function DeterminarDescuentoPorage(age) {
+    if (age > 60) {
+        let descuentoPorage = 0.1
+        return descuentoPorage
     } else {
         return 0
     }
@@ -53,17 +53,17 @@ function mostrarHoraDePedido(hora) {
     }
 }
 
-function calcularValorCubierto(descuentoEnEfectivo,descuentoPorEdad) {
+function calcularValorCubierto(descuentoEnEfectivo,descuentoPorage) {
     let valorDelCubierto = calcularCostoTotal()
     valorDelCubierto  -= valorTotal * descuentoEnEfectivo 
-    valorDelCubierto -= valorTotal * descuentoPorEdad
+    valorDelCubierto -= valorTotal * descuentoPorage
     valorDelCubierto += valorDelCubierto * 0.05
     return valorDelCubierto
 }
 
 function mostrarResumenDelPedido(){
     console.log(`Costo total = ${calcularCostoTotal()}`)
-    console.log(`Descuento aplicado = ${DeterminarDescuentoPorEdad() + DeterminarDescuentoPorMetodoDePago()}`)
+    console.log(`Descuento aplicado = ${DeterminarDescuentoPorage() + DeterminarDescuentoPorMetodoDePago()}`)
     console.log(`Impuesto = ${calcularValorCubierto() -= calcularValorCubierto * 0.05}`)
     console.log(`Costo final = ${calcularValorCubierto()}`)
 }
