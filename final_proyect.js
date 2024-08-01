4d951294f8682ed4a10c73d2086a4ff79332279a
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-08-01 11:01:52 -0300

    feat(final_proyect.js): add verification code to all functions data

--- final_proyect.js
+++ final_proyect.js
@@ -1,30 +1,70 @@
 
-function manageOrdered(name,age,orderQuantity,unitPrice,paymentMethod) {
-    greetConsumer(name)
-    isMayor(age)
-    let totalValue = calculateTotalCost(orderQuantity,unitPrice)
-    let ageDiscount = determineAgeDiscount(age)
+function manageOrdered() {
+    greetConsumer()
+    isMayor()
+    haveStockOrNot() 
+    let product = haveStockOrNot()
+    let totalValue = calculateTotalCost(orderQuantity,product)
+    let ageDiscount = determineAgeDiscount(isMayor())
     let cashDiscount = determineDiscountByPaymentMethod(paymentMethod)
     showOrderTime()
     let coveredValue = calculateValueCovered(cashDiscount,ageDiscount,totalValue)
     let tax = calculateTax(coveredValue)
     showOrderSummary(totalValue, ageDiscount, cashDiscount,coveredValue,tax)
 }
-manageOrdered("Lucas", 66, 2, 500, "Efectivo")
+manageOrdered()
 
 function greetConsumer(name){
-    console.log( `Hola ${name}, bienvenido a Gm2, sera un gusto trabajar juntos!`)
-}
-
-function isMayor(age) {
-    if (age < 18) {
-        console.warn("No puede beber bebidas alcoholicas")
+    let name = window.prompt("Enter your name")
+    if (isNaN(parseFloat(name)) && (name.trim()).length() <= 30) {
+        console.log(name)
+        console.log( `Hi ${name}, welcome to Gm2, it will be a pleasure to work with you!`)
     } else {
-        console.log( "Es mayor de edad")
+        while (!isNaN(parseFloat(name)) || (name.trim()).length() > 30) {
+            name = window.prompt("Enter your name, (30 characters max)")
+        }
+        console.log(`Name: ${name}`)
     }
 }
 
-function calculateTotalCost(orderQuantity,unitPrice) {
+function isMayor(age) {
+    let age = window.prompt("How old are you?")
+    while (isNaN(age) || age > 120 || age < 0){
+        age = window.prompt("Enter your age,(may have more than 0 and less than 120).")
+    }
+    if (age < 18) {
+        console.warn("You cannot drink alcoholic beverages.")
+    } else {
+        console.log( "You are over 18.")
+    }
+    console.log(`Your age: ${age} years.`)
+    return age
+}
+
+function haveStockOrNot(product) {
+    let product = window.prompt("What product will you buy?")
+    product = product.toLowerCase()
+    while (product != "orange" && product != "chicken" && product != "tomato") {
+        product = window.prompt("Enter other product, we dont have that one (orange,chicken,tomato).")
+        product = product.toLowerCase()
+    }
+    console.log(`Product: ${product}`)
+    return product
+}
+
+function calculateTotalCost(orderQuantity) {
+    let orderQuantity = window.prompt(`How much ${product} will you want?`)
+    let unitPrice
+    switch (product) {
+        case "orange":
+            unitPrice = 10
+            break;
+        case "chicken":
+            unitPrice = 30
+            break;
+        case "tomato": 
+            unitPrice = 20
+    }
     return orderQuantity * unitPrice
 } 
 
@@ -37,6 +77,13 @@ function determineAgeDiscount(age) {
 }
 
 function determineDiscountByPaymentMethod(paymentMethod) {
+    let paymentMethod = window.prompt("How will you pay?")
+    paymentMethod = paymentMethod.toLowerCase()
+    while (paymentMethod != "cash" && paymentMethod != "card" && paymentMethod != "transfer") {
+        paymentMethod = window.prompt("How will you pay?")
+        paymentMethod = paymentMethod.toLowerCase()
+
+    }
     if (paymentMethod === "Efectivo") {
         return 0.1
     } else {
