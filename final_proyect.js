12d44a3f956190d9365a9751633107ec8edf0f33
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-08-01 12:08:20 -0300

    fix(final_proyect.js): add haveStock new verification in the while

--- final_proyect.js
+++ final_proyect.js
@@ -1,125 +1,141 @@
-
 function manageOrdered() {
-    greetConsumer()
-    isMayor()
-    haveStockOrNot() 
-    let product = haveStockOrNot()
-    let totalValue = calculateTotalCost(orderQuantity,product)
-    let ageDiscount = determineAgeDiscount(isMayor())
-    let cashDiscount = determineDiscountByPaymentMethod(paymentMethod)
-    showOrderTime()
-    let coveredValue = calculateValueCovered(cashDiscount,ageDiscount,totalValue)
-    let tax = calculateTax(coveredValue)
-    showOrderSummary(totalValue, ageDiscount, cashDiscount,coveredValue,tax)
+  greetConsumer();
+  let age = isOlder();
+  let product = haveStockOrNot();
+  let totalValue = calculateTotalCost(product);
+  let ageDiscount = determineAgeDiscount(age);
+  let cashDiscount = determineDiscountByPaymentMethod();
+  showOrderTime();
+  let coveredValue = calculateValueCovered(
+    cashDiscount,
+    ageDiscount,
+    totalValue
+  );
+  let tax = calculateTax(coveredValue);
+  showOrderSummary(totalValue, ageDiscount, cashDiscount, coveredValue, tax);
 }
-manageOrdered()
+manageOrdered();
 
-function greetConsumer(name){
-    let name = window.prompt("Enter your name")
-    if (isNaN(parseFloat(name)) && (name.trim()).length() <= 30) {
-        console.log(name)
-        console.log( `Hi ${name}, welcome to Gm2, it will be a pleasure to work with you!`)
-    } else {
-        while (!isNaN(parseFloat(name)) || (name.trim()).length() > 30) {
-            name = window.prompt("Enter your name, (30 characters max)")
-        }
-        console.log(`Name: ${name}`)
-    }
-}
-
-function isMayor(age) {
-    let age = window.prompt("How old are you?")
-    while (isNaN(age) || age > 120 || age < 0){
-        age = window.prompt("Enter your age,(may have more than 0 and less than 120).")
-    }
-    if (age < 18) {
-        console.warn("You cannot drink alcoholic beverages.")
-    } else {
-        console.log( "You are over 18.")
-    }
-    console.log(`Your age: ${age} years.`)
-    return age
+function greetConsumer() {
+  let name = window.prompt("Enter your name");
+  const isNameValid = isNaN(parseFloat(name)) && name.trim().length <= 30;
+  while (!isNameValid) {
+    name = window.prompt("Enter your name, (30 characters max)");
+  }
+  console.log(
+    `Hi ${name}, welcome to Gm2, it will be a pleasure to work with you!`
+  );
 }
 
-function haveStockOrNot(product) {
-    let product = window.prompt("What product will you buy?")
-    product = product.toLowerCase()
-    while (product != "orange" && product != "chicken" && product != "tomato") {
-        product = window.prompt("Enter other product, we dont have that one (orange,chicken,tomato).")
-        product = product.toLowerCase()
-    }
-    console.log(`Product: ${product}`)
-    return product
+function isOlder() {
+  let age = window.prompt("How old are you?");
+  while (isNaN(age) || age > 120 || age < 0) {
+    age = window.prompt(
+      "Enter your age,(may have more than 0 and less than 120)."
+    );
+  }
+  if (age < 18) {
+    console.warn("You cannot drink alcoholic beverages.");
+  } else {
+    console.log("You are over 18.");
+  }
+  return age;
 }
 
-function calculateTotalCost(orderQuantity) {
-    let orderQuantity = window.prompt(`How much ${product} will you want?`)
-    let unitPrice
-    switch (product) {
-        case "orange":
-            unitPrice = 10
-            break;
-        case "chicken":
-            unitPrice = 30
-            break;
-        case "tomato": 
-            unitPrice = 20
-    }
-    return orderQuantity * unitPrice
-} 
+function haveStockOrNot() {
+  let product = window.prompt("What product will you buy?").toLowerCase();
+  let haveStock =
+    product != "orange" && product != "chicken" && product != "tomato";
+  while (haveStock) {
+    product = window.prompt(
+      "Enter other product, we dont have that one (orange,chicken,tomato)."
+    );
+    product = product.toLowerCase();
+    haveStock =
+      product != "orange" && product != "chicken" && product != "tomato";
+  }
+  console.log(`Product: ${product}`);
+  return product;
+}
+
+function calculateTotalCost(product) {
+  let orderQuantity = window.prompt(`How much ${product} will you want?`);
+  let unitPrice;
+  switch (product) {
+    case "orange":
+      unitPrice = 10;
+      break;
+    case "chicken":
+      unitPrice = 30;
+      break;
+    case "tomato":
+      unitPrice = 20;
+  }
+  return orderQuantity * unitPrice;
+}
 
 function determineAgeDiscount(age) {
-    if (age > 60) {
-        return 0.1
-    } else {
-        return 0
-    }
+  if (age > 60) {
+    return 0.1;
+  } else {
+    return 0;
+  }
 }
 
-function determineDiscountByPaymentMethod(paymentMethod) {
-    let paymentMethod = window.prompt("How will you pay?")
-    paymentMethod = paymentMethod.toLowerCase()
-    while (paymentMethod != "cash" && paymentMethod != "card" && paymentMethod != "transfer") {
-        paymentMethod = window.prompt("How will you pay?")
-        paymentMethod = paymentMethod.toLowerCase()
-
-    }
-    if (paymentMethod === "cash") {
-        return 0.1
-    } else {
-        return 0
-    }
+function determineDiscountByPaymentMethod() {
+  let paymentMethod = window.prompt("How will you pay?").toLowerCase();
+  let isPaymentMethodValid =
+    paymentMethod != "cash" &&
+    paymentMethod != "card" &&
+    paymentMethod != "transfer";
+  while (isPaymentMethodValid) {
+    paymentMethod = window.prompt("How will you pay?");
+    paymentMethod = paymentMethod.toLowerCase();
+  }
+  if (paymentMethod === "cash") {
+    return 0.1;
+  } else {
+    return 0;
+  }
 }
 
 function showOrderTime() {
-    let now = new Date()
-    let hours = now.getHours()
-    let minutes = now.getMinutes()
-    if (hours >= 12 && hours <= 14 ) {
-        if (hours === 14 && minutes === 0) {
-            console.log( "There may be delays as the order was placed during peak hours.")
-        } else if (hours < 14) {
-            console.log( "There may be delays as the order was placed during peak hours.")
-        }
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (hours >= 12 && hours <= 14) {
+    if (hours === 14 && minutes === 0) {
+      console.log(
+        "There may be delays as the order was placed during peak hours."
+      );
+    } else if (hours < 14) {
+      console.log(
+        "There may be delays as the order was placed during peak hours."
+      );
     }
+  }
 }
 
-function calculateValueCovered(cashDiscount,ageDiscount,totalValue) {
-    let coveredValue = totalValue
-    coveredValue  -= totalValue * cashDiscount 
-    coveredValue -= totalValue * ageDiscount
-    return coveredValue
+function calculateValueCovered(cashDiscount, ageDiscount, totalValue) {
+  let coveredValue = totalValue;
+  coveredValue -= totalValue * cashDiscount;
+  coveredValue -= totalValue * ageDiscount;
+  return coveredValue;
 }
 
-function calculateTax(coveredValue){
-    return coveredValue * 0.05
+function calculateTax(coveredValue) {
+  return coveredValue * 0.05;
 }
 
-function showOrderSummary(totalValue, ageDiscount, cashDiscount,coveredValue,tax){
-    console.log(`Costo total = ${totalValue}`)
-    console.log(`Descuento aplicado = ${ageDiscount + cashDiscount}`)
-    console.log(`Impuesto = ${tax}`)
-    console.log(`Costo final = ${coveredValue + tax}`)
+function showOrderSummary(
+  totalValue,
+  ageDiscount,
+  cashDiscount,
+  coveredValue,
+  tax
+) {
+  console.log(`Total value = ${totalValue}`);
+  console.log(`Discount apply = ${ageDiscount + cashDiscount}`);
+  console.log(`Tax = ${tax}`);
+  console.log(`Final cost = ${coveredValue + tax}`);
 }
-
-
