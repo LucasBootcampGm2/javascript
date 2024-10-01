949822babf769c9b50a31b1bd6b90a48094ddf23
Lucas <lcirilloberardi@alumno.huergo.edu.ar>
2024-09-30 22:12:12 -0300

    feat(fetch.js): play with pokemon api to practice fetch

--- asynchronous/promises/landingRandom.js
+++ asynchronous/promises/landingRandom.js
@@ -1,32 +1,32 @@
 // function createPageStructure() {
 //     return new Promise((resolve) => {
-//       let body = document.getElementById("body");
+//       const body = document.getElementById("body");
 
-//       let header = document.createElement("header");
+//       const header = document.createElement("header");
 //       header.classList.add("header");
 
-//       let h1 = document.createElement("h1");
+//       const h1 = document.createElement("h1");
 //       h1.textContent = "Welcome to my random num generator";
 //       header.append(h1);
 
-//       let main = document.createElement("main");
+//       const main = document.createElement("main");
 //       main.setAttribute("id", "main");
 
-//       let loading = document.createElement("h2");
+//       const loading = document.createElement("h2");
 //       loading.textContent = "Ready to generate";
 
-//       let container = document.createElement("div");
+//       const container = document.createElement("div");
 //       container.classList.add("container");
 
-//       let h2 = document.createElement("h2");
+//       const h2 = document.createElement("h2");
 //       h2.textContent = "Click the button to generate the random num";
 //       h2.classList.add("h2");
 
-//       let span = document.createElement("span");
+//       const span = document.createElement("span");
 //       span.classList.add("random-span");
 //       span.textContent = "0";
 
-//       let button = document.createElement("button");
+//       const button = document.createElement("button");
 //       button.setAttribute("id", "random-button");
 //       button.textContent = "random";
 
@@ -50,15 +50,15 @@
 
 //   async function changeRandomNumSpan() {
 //     try {
-//       let button = await createPageStructure();
+//       const button = await createPageStructure();
 
 //       button.addEventListener("click", async function () {
-//         let loading = document.querySelector("h2");
+//         const loading = document.querySelector("h2");
 //         loading.textContent = "Generating random num...";
 
-//         let randomNum = await createRandomNum();
+//         const randomNum = await createRandomNum();
 //         setTimeout(() => {
-//           let randomSpan = document.querySelector(".random-span");
+//           const randomSpan = document.querySelector(".random-span");
 //           randomSpan.textContent = randomNum;
 //           loading.textContent = "Random number generated!";
 //         }, 3000);
@@ -74,33 +74,33 @@
 
 function createPageStructure() {
   return new Promise((resolve) => {
-    let body = document.getElementById("body")
+    const body = document.getElementById("body")
 
-    let header = document.createElement("header")
+    const header = document.createElement("header")
     header.classList.add("header")
 
-    let h1 = document.createElement("h1")
+    const h1 = document.createElement("h1")
     h1.textContent = "Welcome to my random num generator"
     header.append(h1)
 
-    let main = document.createElement("main")
+    const main = document.createElement("main")
     main.setAttribute("id", "main")
 
-    let loading = document.createElement("h2")
+    const loading = document.createElement("h2")
     loading.textContent = "Ready to generate"
 
-    let container = document.createElement("div")
+    const container = document.createElement("div")
     container.classList.add("container")
 
-    let h2 = document.createElement("h2")
+    const h2 = document.createElement("h2")
     h2.textContent = "Click the button to generate the random num"
     h2.classList.add("h2")
 
-    let span = document.createElement("span")
+    const span = document.createElement("span")
     span.classList.add("random-span")
     span.textContent = "0"
 
-    let button = document.createElement("button")
+    const button = document.createElement("button")
     button.setAttribute("id", "random-button")
     button.textContent = "random"
 
@@ -118,14 +118,14 @@ function createPageStructure() {
 
 function createRandomNum() {
   return new Promise((resolve) => {
-    let randomNum = Math.floor(Math.random() * 10 + 1)
+    const randomNum = Math.floor(Math.random() * 10 + 1)
     resolve(randomNum)
   })
 }
 
 function updateLoadingText(text) {
   return new Promise((resolve) => {
-    let loading = document.querySelector("h2")
+    const loading = document.querySelector("h2")
     loading.textContent = text
     resolve()
   })
@@ -134,7 +134,7 @@ function updateLoadingText(text) {
 function updateRandomSpan(randomNum) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      let randomSpan = document.querySelector(".random-span")
+      const randomSpan = document.querySelector(".random-span")
       randomSpan.textContent = randomNum
       resolve()
     }, 3000)
