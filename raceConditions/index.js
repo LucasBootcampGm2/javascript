4173f19be09fbce55188c698abed9e512c98c5ef
Lucas Cirillo Berardi <lucascirilloberardi@HP-ProBook-440-G8-Notebook-PC-c0332be8.localdomain>
2024-10-15 10:46:17 +0000

    feat(raceConditions/index.js): delete the comment with the exercise

--- raceConditions/index.js
+++ raceConditions/index.js
@@ -1,31 +1,3 @@
-// Imagina que estás desarrollando una plataforma de venta de entradas para un evento en línea. Tenes un sistema básico donde los usuarios pueden comprar entradas. Sin embargo, varias personas pueden intentar comprar la misma cantidad de entradas al mismo tiempo, lo que puede provocar inconsistencias en la cantidad de entradas disponibles debido a las “race conditions”.
-
-// Requerimientos:
-
-// Hay un total de 10 entradas disponibles.
-
-// Varias solicitudes pueden llegar al servidor al mismo tiempo para comprar entradas.
-
-// El sistema debe garantizar que no se vendan más de las 10 entradas disponibles.
-
-// Tareas:
-
-// Escribir una simulación en JavaScript (puede ser en un entorno Node.js) donde múltiples usuarios intenten comprar entradas al mismo tiempo.
-
-// Usar la función setTimeout para simular las solicitudes concurrentes. Cada usuario intentará comprar entradas con un retraso aleatorio.
-
-// Crear una función que se encargue de la compra, que reciba la cantidad de entradas solicitadas y actualice el número total de entradas disponibles.
-
-// Detectar el problema: Ver qué sucede cuando varios usuarios intentan comprar al mismo tiempo y cómo las entradas pueden ser vendidas en exceso (race condition).
-
-// Solucionar la race condition: Proponer y aplicar una solución para evitar que las entradas disponibles se vendan en exceso. Se puede utilizar una de las siguientes estrategias:
-
-// Uso de bloqueos (locks) para asegurar que solo una solicitud pueda procesar la compra a la vez.
-
-// Implementar algún tipo de sincronización entre las solicitudes.
-
-// Alternativamente, puedes implementar promesas que aseguren que la actualización del stock sea atómica.
-
 // function buyTickets(quantity, user) {
 //   console.log(`${user} is buying ${quantity} tickets...`);
 
