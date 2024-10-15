9b7098eac66e978c5d6e38a378c9f43af9a035ff
Lucas Cirillo Berardi <lucascirilloberardi@HP-ProBook-440-G8-Notebook-PC-c0332be8.localdomain>
2024-10-15 10:45:14 +0000

    feat(raceConditions/index.js): make the correct logic of the tickets purchase

--- raceConditions/index.js
+++ raceConditions/index.js
@@ -26,26 +26,64 @@
 
 // Alternativamente, puedes implementar promesas que aseguren que la actualización del stock sea atómica.
 
+// function buyTickets(quantity, user) {
+//   console.log(`${user} is buying ${quantity} tickets...`);
+
+//   if (availableTickets >= quantity) {
+//     setTimeout(() => {
+//       availableTickets -= quantity;
+//       console.log(
+//         `User ${user} bought ${quantity} tickets. Remaining tickets: ${availableTickets}`
+//       );
+//     }, Math.random() * 100);
+//   } else {
+//     console.log(`User ${user} could not buy tickets. Not enough available.`);
+//   }
+// }
+
+// setTimeout(() => {
+//   buyTickets(4, "Lucas");
+//   buyTickets(3, "Franco");
+//   buyTickets(2, "Matias");
+//   buyTickets(3, "Seba");
+// }, Math.random() * 100);
+
 let availableTickets = 10;
 
 function buyTickets(quantity, user) {
-  console.log(`${user} is buying ${quantity} tickets...`);
+  return new Promise((resolve, reject) => {
+    console.log(`${user} is buying ${quantity} tickets...`);
 
-  if (availableTickets >= quantity) {
-    setTimeout(() => {
-      availableTickets -= quantity;
-      console.log(
-        `User ${user} bought ${quantity} tickets. Remaining tickets: ${availableTickets}`
-      );
-    }, Math.random() * 100);
-  } else {
-    console.log(`User ${user} could not buy tickets. Not enough available.`);
+    if (availableTickets > 0) {
+      setTimeout(() => {
+        if (availableTickets - quantity < 0) {
+          reject(
+            `They are only ${availableTickets} avialable so ${user} only buy that quantity`
+          );
+          availableTickets = 0;
+        } else {
+          availableTickets -= quantity;
+          console.log(
+            `User ${user} bought ${quantity} tickets. Remaining tickets: ${availableTickets}`
+          );
+          resolve();
+        }
+      }, Math.random() * 100);
+    } else {
+      reject(`User ${user} tried to buy tickets but there are not enough`);
+    }
+  });
+}
+
+async function purchaseTickets() {
+  try {
+    await buyTickets(4, "Lucas");
+    await buyTickets(3, "Seba");
+    await buyTickets(5, "Franco");
+    await buyTickets(6, "Matias");
+  } catch (error) {
+    console.error("Error", error);
   }
 }
 
-setTimeout(() => {
-  buyTickets(4, "Lucas");
-  buyTickets(3, "Franco");
-  buyTickets(2, "Matias");
-  buyTickets(3, "Seba");
-}, Math.random() * 100);
+purchaseTickets();
